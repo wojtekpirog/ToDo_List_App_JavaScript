19cae15190bc808a909c4ed1c77fc49c18cc3aef
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-01-05 21:24:09 +0100

    Create function that add an area with tools for checking, editing and removing tasks

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let errorInfo; // Info o braku zadań
 let addBtn; // Przycisk ADD
 let ulList; // Lista zadań
 let newTask; // Nowe zadanie (element li)
+// let toolsArea; // Div przechowujący przyciski "'✔️', 'EDIT' oraz '❌'"
+// let checkmarkButton; // Przycisk '✔️'
+// let editButton; // Przycisk 'EDIT' 
+// let cancelButton; // Przycisk '❌'
+
+const main = () => {
+  prepareDOMElements();
+  prepareDOMEvents();
+}
 
 const prepareDOMElements = () => {
   // Tu pobieram elementy
@@ -15,12 +24,18 @@ const prepareDOMElements = () => {
 const prepareDOMEvents = () => {
   // Tu nadaję nasłuchiwanie
   addBtn.addEventListener("click", addTodo);
+  todoInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+      addTodo();
+    }
+  });
 }
 
 const addTodo = () => {
   if (todoInput.value !== "") {
     newTask = document.createElement("li");
     newTask.textContent = todoInput.value;
+    addToolsArea();
     ulList.appendChild(newTask); 
     todoInput.value = "";
     errorInfo.textContent = "";     
@@ -30,9 +45,22 @@ const addTodo = () => {
   }
 }
 
-const main = () => {
-  prepareDOMElements();
-  prepareDOMEvents();
+const addToolsArea = () => {
+  const toolsArea = document.createElement("div");
+  toolsArea.classList.add("tools");
+
+  const checkmarkButton = document.createElement("button");
+  checkmarkButton.innerHTML = "<i class='fas fa-check lime'></i>";
+
+  const editButton = document.createElement("button");
+  editButton.textContent = "EDIT";
+  editButton.classList.add("bold");
+
+  const cancelButton = document.createElement("button");
+  cancelButton.innerHTML = "<i class='fas fa-times tomato'></i>";
+
+  toolsArea.append(checkmarkButton, editButton, cancelButton);
+  newTask.appendChild(toolsArea);
 }
 
 document.addEventListener("DOMContentLoaded", main);
